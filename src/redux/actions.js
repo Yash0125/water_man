b591eccaa21f06522e11774ba4a50e2dbc7bcfac
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-06-09 22:55:57 +0530

    feat : redux

---
+++ src/redux/actions.js
@@ -0,0 +1,7 @@
+export const updateValue = (value) => {
+    return {
+      type: "UPDATE_VALUE",
+      payload: value
+    };
+  };
+  
---
+++ src/redux/reducer.js
@@ -0,0 +1,18 @@
+const initialState = {
+    value: ""
+  };
+  
+  const reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case "UPDATE_VALUE":
+        return {
+          ...state,
+          value: action.payload
+        };
+      default:
+        return state;
+    }
+  };
+  
+  export default reducer;
+  
