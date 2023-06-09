2debef28dd5c5abb46ae6914952689caaad3ce6a
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-06-09 22:52:38 +0530

    feat : ComponentA

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import ComponentA from './components/ComponentA';
 
 function App() {
   return (
     <div className="App">
-      
+      <ComponentA/>
     </div>
   );
 }
